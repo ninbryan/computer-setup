7863de68d26561f54e4231e17e6cbd92bb749e6d
Bryan Maamo <ninbryan@users.noreply.github.com>
2017-10-24 15:53:25 -0700

    Update ubuntu-install-nvm.bash

--- ubuntu-install-nvm.bash
+++ ubuntu-install-nvm.bash
@@ -1,11 +1,6 @@
 # TODO - need to test
-
-if [ ! $(command -v nvm) ] ; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
-  echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc
-  echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> $HOME/.bashrc
-  source $HOME/.bashrc
-fi
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+source $HOME/.bashrc
 
 echo "nvm $(nvm -v)"
 
