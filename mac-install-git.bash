e259f69312076e26aa70f39dfe9dc55074327a33
Bryan Maamo <ninbryan@users.noreply.github.com>
2017-11-08 17:43:06 -0700

    Update mac-install-git.bash

--- mac-install-git.bash
+++ mac-install-git.bash
@@ -1,19 +1,16 @@
-
 #!/bin/bash
 
+echo "running mac-install-git.bash script"
+
 if [$(command -v brew) == ""]; then
-  # install brew
+  echo "--- downloading and installing brew ---"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  # brew exists
+  echo "--- brew exists ---"
   echo "$(brew -v)"
 fi
 
-if [$(command -v git) == ""]; then
-  brew install git
-fi
+brew install git
 
 echo "git path: $(command -v git)"
 echo $(git --version)
-
-# man git
